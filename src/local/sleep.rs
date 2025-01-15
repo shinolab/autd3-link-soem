1bcf7fa99e9709bd0158e3ef82aad9feae867552
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-16 08:33:30 +0900

    bump to v29.0.0-rc.16

--- src/local/sleep.rs
+++ src/local/sleep.rs
@@ -1,29 +1,5 @@
+use autd3_core::utils::timer::TimerResolutionGurad;
 use std::{num::NonZeroU32, time::Duration};
-
-#[doc(hidden)]
-#[cfg_attr(not(target_os = "windows"), allow(dead_code))]
-pub struct TimerResolutionGurad {
-    timer_resolution: Option<NonZeroU32>,
-}
-
-impl TimerResolutionGurad {
-    pub fn new(timer_resolution: Option<NonZeroU32>) -> Self {
-        #[cfg(target_os = "windows")]
-        timer_resolution.map(|timer_resolution| unsafe {
-            windows::Win32::Media::timeBeginPeriod(timer_resolution.get())
-        });
-        Self { timer_resolution }
-    }
-}
-
-impl Drop for TimerResolutionGurad {
-    fn drop(&mut self) {
-        #[cfg(target_os = "windows")]
-        self.timer_resolution.map(|timer_resolution| unsafe {
-            windows::Win32::Media::timeEndPeriod(timer_resolution.get())
-        });
-    }
-}
 
 pub(crate) trait Sleep {
     fn sleep(duration: Duration);
